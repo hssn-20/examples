05c4499200db9e19f5c0804c58bd5f621a25cef1
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2024-01-02 18:38:38 +0000

    adding the new dataset

--- bert/src/convert_dataset.py
+++ bert/src/convert_dataset.py
@@ -163,23 +163,23 @@ virusconstants = DatasetConstants(
 )
 virusconstants.splits['train'] = DataSplitConstants(hf_split='train',
                                                  folder_split='train',
-                                                 raw_samples=2_602_437,
+                                                 raw_samples=44285,
                                                  truncated_samples=None)
 # virusconstants.splits['train_small'] = DataSplitConstants(
 #     hf_split='train',
 #     folder_split='train_small',
 #     raw_samples=1000000,
 #     truncated_samples=100000)
-# virusconstants.splits['val'] = DataSplitConstants(hf_split='validation',
-#                                                folder_split='val',
-#                                                raw_samples=364608,
-#                                                truncated_samples=None)
+virusconstants.splits['val'] = DataSplitConstants(hf_split='valid',
+                                               folder_split='val',
+                                               raw_samples=5536,
+                                               truncated_samples=None)
 # virusconstants.splits['val_small'] = DataSplitConstants(hf_split='validation',
 #                                                      folder_split='val_small',
 #                                                      raw_samples=10000,
 #                                                      truncated_samples=10000)
 
-CONSTS = {'c4': c4constants, 'the_pile': pileconstants, 'Hack90/virus_dna_dataset': virusconstants}
+CONSTS = {'c4': c4constants, 'the_pile': pileconstants, 'Hack90/virus_10kbp_50kbp': virusconstants}
 
 
 class NoConcatDataset(IterableDataset):
